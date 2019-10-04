68578a22635ef3e656d17e5612c554bef0c8b92a
Jacob Williams <JaWilliamsDev@gmail.com>
2019-10-03 21:34:25 -0400

    the dog website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Img = styled.img`
+  max-height: 250px;
+  max-width: 250px;
+  padding: 10px;
+`
+
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      breed: "husky",
+      images: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchDogImgs()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.breed !== this.state.breed){
+    this.setState({
+      images: []
+    })
+
+    this.fetchDogImgs()}
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      breed: event.target.value
+    });
+  };
+
+  fetchDogImgs = () => {
+    axios
+    .get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
+    .then(res => {
+      this.setState({
+        images: res.data.message
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>The Dog Website</h1>
+        <select value={this.state.breed} onChange={this.handleChange}>
+          <option value="husky">Husky</option>
+          <option value="beagle">Beagle</option>
+          <option value="corgi">Corgi</option>
+          <option value='boxer'>Boxer</option>
+          <option value='dane/great'>Great Dane</option>
+        </select>
+        <div>
+          {this.state.images.map((image, index) => (
+            <Img key={index} src={image} alt="dog" />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
